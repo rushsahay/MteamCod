c87eceaa42a874b7beecd4ef011b49ad9b6670b0
ezan13 <fumakeyaezan@gmail.com>
2024-10-25 00:15:22 -0500

    Update robot-config.h
    new driver controls

--- include/robot-config.h
+++ include/robot-config.h
@@ -7,16 +7,28 @@ using namespace vex;
  *
  * This should be called at the start of your int main function.
  */
- extern motor left1;
- extern motor left2;
- extern motor left3;
- extern motor_group leftMotors;
- extern motor right1;
- extern motor right2;
- extern motor right3;
- extern motor_group rightMotors;
- extern drivetrain drive;
- extern controller driver;
+// //  extern motor left1;
+//  extern motor left2;
+//  extern motor left3;
+//  extern motor_group leftMotors;
+//  extern motor right1;
+//  extern motor right2;
+//  extern motor right3;
+//  extern motor_group rightMotors;
+//  extern drivetrain drive;
+//  extern controller driver;
+
+extern controller Controller1;
+extern motor leftMotorA;
+extern motor leftMotorB;
+extern motor_group LeftDriveSmart;
+extern motor rightMotorA;
+extern motor rightMotorB;
+extern motor_group RightDriveSmart;
+extern drivetrain Drivetrain;
  extern digital_out piston;
- extern motor contake;
+ extern motor conveyor;
+ extern motor intake;
+ extern motor_group contake;
+ int rc_auto_loop_function_Controller1();
 void vexcodeInit(void);
